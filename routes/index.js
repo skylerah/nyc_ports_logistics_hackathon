ad2e0af1123c7a8cb0712f984bdefe3cc7cbaac8
Skyler Hartle <skyler@shapesocial.net>
2017-01-22 12:58:04 -0500

    another big commit

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
   request.get('https://betaapi.azuqua.com/flo/6684719c2d94797815a095ab2c305777/invoke?clientToken=87bdf3281c6b7b8fb4bed6f0b5babb2f8b5625fc2e2251c8f2aec84004d237ca', function(error, response, body) {
     if (error) console.error(error);
     var parsedJson = {};
+    var objectArray = [];
     try {
       parsedJson = JSON.parse(body);
     } catch (e) {
@@ -21,6 +22,7 @@ router.get('/', function(req, res, next) {
         continue;
     }
       var instance = parsedJson.Response.Output[key];
+      var totalTimePassed = 0;
       var stages = instance.stages;
       for (var key2 in stages) {
         if (!stages.hasOwnProperty(key2)) {
@@ -28,15 +30,18 @@ router.get('/', function(req, res, next) {
         }
         var stage = stages[key2];
         var now = moment();
-        console.log(stage.start_time);
-        var stageTime = moment('2016-06-06T21:03:55');
-        var timePassed = now.diff(stageTime, 'hours');
-        console.log(stageTime, now, timePassed);
+        var stageTime = moment(stage.start_time);
+        var stageTimePassed = now.diff(stageTime, 'hours');
 
+        totalTimePassed += stageTimePassed;
       }
+      instance.passedTime = totalTimePassed;
+      objectArray.push(instance);
+      console.log(instance);
+
     }
+    res.render('index', {instances: objectArray});
   });
-  res.render('index', {});
 });
 
 module.exports = router;
--- routes/instances.js
+++ routes/instances.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var moment = require('moment');
 
 router.get('/:id', function(req, res) {
-  res.render('instances/index');
+  request.get('https://betaapi.azuqua.com/flo/c974fa912d0d58a2736ca7db9cda8743/invoke?uuid=' + req.params.id, function(error, response, body) {
+    if (error) console.error(error);
+
+    var parsedJson = {};
+    try {
+      parsedJson = JSON.parse(body);
+    } catch (e) {
+      console.error(e);
+    }
+    var stages = parsedJson.Response.Output.stages;
+
+    for (var key in stages) {
+      if (!stages.hasOwnProperty(key)) {
+        continue;
+      }
+      var startTime = stages[key].start_time;
+      var now = moment();
+      var stageTimePassed = now.diff(startTime, 'hours');
+      stages[key].time_passed = stageTimePassed;
+    }
+
+
+    console.log(stages);
+    res.render('instances/index', {stages: stages, id: req.params.id});
+  });
 });
 
 module.exports = router;
